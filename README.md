2ee5f2f99f0e43a297e3f08081f89cb28dd81c47
AnatolyKoptelov <7497490@gmail.com>
2018-05-18 13:37:48 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Application for finding duplicated files on your filesystem. Anti-Duplicator searches into subdirectories of your custom path.
+Give a directory path  by positional argument and get list of duplicated files, whith same name and size.
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.*:
+
+```
+python duplicates.py path
+
+```
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,51 @@
+import os
+import sys
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Find and print file duplicates'
+    )
+    parser.add_argument(
+        'path',
+        metavar='path',
+        type=str,
+        help='File path',
+    )
+    return parser.parse_args()
+
+
+def collect_filenames(path):
+    directories = [path, ]
+    files = {}
+    while directories:
+        for directory in directories:
+            for dir_entry in os.scandir(path=directory):
+                if dir_entry.is_dir():
+                    directories.append(dir_entry.path)
+                elif dir_entry.is_file():
+                    name_size_unique_key = '{}*{}'.format(
+                        dir_entry.name,
+                        os.path.getsize(dir_entry.path),
+                    )
+                    if files.get(name_size_unique_key, False):
+                        files[name_size_unique_key].append(dir_entry.path)
+                    else:
+                        files[name_size_unique_key] = [dir_entry.path, ]
+            directories.remove(directory)
+    return files
+
 
 if __name__ == '__main__':
-    pass
+    path = get_args().path
+    if not os.path.isdir(path):
+        sys.exit('Path must be a directory')
+    files = collect_filenames(path)
+    for name_size_unique_key, paths in files.items():
+        if len(paths) > 1:
+            print('File name: {} \t File size: {} \t'. format(
+                *name_size_unique_key.split('*'),
+            ))
+            for path in paths:
+                print('\t{}'.format(path))
