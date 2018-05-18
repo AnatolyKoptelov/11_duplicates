32ab763e57bb5a058933c9b8374708412d980386
AnatolyKoptelov <7497490@gmail.com>
2018-05-18 15:56:19 +0300

    Simplify function `collect_filenames`

--- duplicates.py
+++ duplicates.py
@@ -16,25 +16,44 @@ def get_args():
     return parser.parse_args()
 
 
-def collect_filenames(path):
-    directories = [path, ]
+def analyze_directory_content(directory):
+    directories = []
     files = {}
-    while directories:
-        for directory in directories:
-            for dir_entry in os.scandir(path=directory):
-                if dir_entry.is_dir():
-                    directories.append(dir_entry.path)
-                elif dir_entry.is_file():
-                    name_size_unique_key = '{}*{}'.format(
-                        dir_entry.name,
-                        os.path.getsize(dir_entry.path),
-                    )
-                    if files.get(name_size_unique_key, False):
-                        files[name_size_unique_key].append(dir_entry.path)
-                    else:
-                        files[name_size_unique_key] = [dir_entry.path, ]
-            directories.remove(directory)
-    return files
+    for dir_entry in os.scandir(path=directory):
+        if dir_entry.is_dir():
+            directories.append(dir_entry.path)
+        elif dir_entry.is_file():
+            name_size_unique_key = '{}*{}'.format(
+                dir_entry.name,
+                os.path.getsize(dir_entry.path),
+            )
+            files[name_size_unique_key] = dir_entry.path
+    return directories, files
+
+
+def add_searched(searched_files, files):
+    for name_size_unique_key in files.keys():
+        if searched_files.get(name_size_unique_key, False):
+            searched_files[name_size_unique_key].append(
+                files[name_size_unique_key],
+            )
+        else:
+            searched_files[name_size_unique_key] = [
+                files[name_size_unique_key],
+            ]
+    return searched_files
+
+
+def collect_filenames(path):
+    directories_for_serching = [path, ]
+    searched_files = {}
+    while directories_for_serching:
+        for directory in directories_for_serching:
+            directories, files = analyze_directory_content(directory)
+            directories_for_serching.extend(directories)
+            searched_files = add_searched(searched_files, files)
+            directories_for_serching.remove(directory)
+    return searched_files
 
 
 if __name__ == '__main__':
