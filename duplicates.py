24ca80e9d97a4855b1bcaae36ba3e91d015b3a37
AnatolyKoptelov <7497490@gmail.com>
2018-05-30 11:29:04 +0300

    Add cross-platform support

--- duplicates.py
+++ duplicates.py
@@ -17,27 +17,28 @@ def get_args():
     return parser.parse_args()
 
 
-def collect_filenames(path):
-    searched_files = collections.defaultdict(list)
-    for base_path, directories, files in os.walk(path):
-        for file in files:
-            full_pass = '{}/{}'.format(base_path, file)
-            searched_files[(
-                file,
-                os.path.getsize(full_pass)
-            )].append(full_pass)
-    return searched_files
+def collect_files_locations(path):
+    files_locations = collections.defaultdict(list)
+    for base_path, directories, file_names in os.walk(path):
+        for file_name in file_names:
+            full_path = os.path.join(base_path, file_name)
+            files_locations[(
+                file_name,
+                os.path.getsize(full_path)
+            )].append(full_path)
+    return files_locations
 
 
 if __name__ == '__main__':
     path = get_args().path
     if not os.path.isdir(path):
         sys.exit('Path must be a directory')
-    files = collect_filenames(path)
-    for name_size_unique_key, paths in files.items():
+    files_locations = collect_files_locations(path)
+    for (file_name, file_size), paths in files_locations.items():
         if len(paths) > 1:
             print('File name: {} \t File size: {} \t'. format(
-                *name_size_unique_key,
+                file_name,
+                file_size,
             ))
             for path in paths:
                 print('\t{}'.format(path))
